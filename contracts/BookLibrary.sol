fa06af272e0a125ff3581fc77af8c55683590949
baltasarromero <baltasarromero@gmail.com>
2023-02-07 10:27:43 -0300

    Applied feedback from weekly session

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.7;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
 
 /**
 @dev Import the Ownable contract.
@@ -8,98 +8,211 @@ import "./Ownable.sol";
 
 /**
 	@title BookLibrary
-	@notice This contract implements a library where books can be added, borrowed, and returned. It also allows to view the whole list of books and the complete
-	list of borrowers for  a given  book
+	@notice This contract implements a library where books can be added, borrowed, and returned. It allows client to get a book by id or title and it also allows 
+    to view the whole list of borrowers for  a given  book
 	@dev  It inherits from the Ownable contract to ensure that only the owner can add books.
+	@dev Assumption: it is not a requirement that the list of borrowers of a given book doesn't repeat borrower addresses. i.e. if an address borrows a book multiple times the address will be added to the borrowers array multiple times
+	@dev Removing books from the library is not a requirement
 	@author Baltasar Romero
 */
 contract BookLibrary is Ownable {
-	///@dev Mapping of books in the library. The key is the book ID and the value is the book information.
-	mapping(uint => Book) public books;
-	///@dev Mapping of current book borrowers. The first key is the book ID, the second key is the borrower address, and the value is a boolean indicating whether the book is currently borrowed
-	///	by that address.
-	mapping (uint => mapping(address => bool)) public currentBookBorrowers;
-	///@dev Mapping of historical book borrowers. The first key is the book ID, the second key is the borrower address, and the value is a boolean indicating whether the book is currently borrowed
-	///	by that address.
-	mapping (uint => mapping(address => bool)) public historicalBookBorrowers;
-	/// @dev Counter to keep track of the number of books in the library.
-	uint public bookCount;
+    ///	@dev Struct that defines the book information. In includes the following data: the Title of the book, the number of copies currently available in the library, Array with addresses of the borrowers (notice this won't be unique).
+    struct Book {
+        string title;
+        uint256 copies;
+        address[] borrowers;
+    }
 
-	///	@dev Struct that defines the book information.
-	/// @dev Title of the book.
-	/// @dev Total number of copies of the book.
-	/// @dev Number of borrowed copies of the book.
-	/// @dev Array of addresses of the borrowers.
-	struct Book {
-		string title;
-		uint copies;
-		uint borrowedCopies;
-		address[] borrowers;
-	}
+    ///@dev Mapping of books in the library. The key is the book title in bytes32 as it is more gas efficient.
+    mapping(bytes32 => Book) public books;
 
-	/// @dev Function to add a book to the library. Only the owner can add a book.
-	/// @param _title Title of the book.
-	/// @param _copies Total number of copies of the book.
-	function addBook(string memory _title, uint _copies) public onlyOwner { 
-		books[bookCount] =  Book({
-			title: _title,
-			copies: _copies,
-			borrowedCopies: 0,
-			borrowers: new address[](0)
-		});
-		bookCount++;
-	}
+    /// @dev Contains all the different book keys (based on their title) this can be used by clients to get all the keys and then retrieve all the books
+    bytes32[] public bookKeys;
 
-	/// @dev Function to view the books in the library.
-	/// @return Array of book titles and array of available copies of each book.
-	function viewBooks() public view returns (string[] memory, uint[] memory) {
-		string[] memory titles = new string[](bookCount);
-		uint[] memory copies = new uint[](bookCount);
-		for (uint i = 0; i < bookCount; i++) {
-			titles[i] = books[i].title;
-			copies[i] = books[i].copies - books[i].borrowedCopies;
-		}
-		return (titles, copies);
-	}
+    /// @dev Mapping of books currently borrowed to the borrowers. The first key is the address of the borrower, the second key is the book key (title in bytes32) and the value is a boolean indicating whether the address is currently borrowing the book
+    mapping(address => mapping(bytes32 => bool)) public borrowedBook;
+
+    /// @dev This is an event that will be emitted everytime a book title is added to the library for the first time. 
+    event NewBookAdded(string title);
+    /// @dev Event to be emmited when new copies of an already existent book are added to the library
+    event BookCopyAdded(string title, uint256 copies);
+    /// @dev Emmited whenever a copy of a given book is emmited
+    event BookBorrowed(string title, address borrower, uint256 availableCopies);
+    /// @dev Emmited when a copy of a book is returned to the library
+    event BookReturned(string title, address borrower, uint256 availableCopies);
+
+    /**
+     * @dev this is used to validate that a book that is being added to the library has a valid title. THe title should not be empty     .
+     * @param   _title  the title of the book that we want to validate
+     */
+    modifier validBookTitle(string memory _title) {
+        bytes memory tempTitle = bytes(_title);
+        require(tempTitle.length > 0, "Title is not valid");
+        _;
+    }
+
+    /**
+     * @notice  checks if a book with the given title is already present in the library
+     * @dev     converts the string parameter to bytes, looks in the mapping and does the comparison by checking the title's bytes length
+     * @param   _title  the title of the book we are looking for
+     * @return exists a boolean value indicating if the book is present in the library
+     * @return bookKey the title of the book in bytes32 format that can be used as key for lookup
+     * @return requestedBook the struct with the data associated with the requeted book. If the book is not found all values will be set to its default
+     */
+    function findBook(string memory _title)
+        private
+        view
+        validBookTitle(_title)
+        returns (
+            bool exists,
+            bytes32 bookKey,
+            Book storage requestedBook
+        )
+    {
+        bookKey = keccak256(abi.encodePacked(_title));
+        requestedBook = books[bookKey];
+
+        exists = bytes(requestedBook.title).length != 0;
+    }
+
+    /// @dev Function to add a copy of a given book to the library. Only the owner can add a book. The title cannot be empty. Only the owner of the contract can invoke this function.
+    /// @param _title Title of the book.
+    function addBook(string memory _title)
+        public
+        onlyOwner
+        validBookTitle(_title)
+    {
+        (bool exists, bytes32 bookKey, Book storage book) = findBook(_title);
+
+        // if book does not exist we should insert it
+        // if it already exist we should just increase the number of copies
+        if (!exists) {
+            Book memory newBook = Book({
+                title: _title,
+                copies: 1,
+                borrowers: new address[](0)
+            });
+            books[bookKey] = newBook;
+            bookKeys.push(bookKey);
+        } else {
+            book.copies++;
+        }
+
+        // Emit events
+        if (!exists) {
+            emit NewBookAdded(_title);
+        } else {
+            emit BookCopyAdded(_title, book.copies);
+        }
+    }
+
+    ///	@dev Allows a user to borrow a book from the library. Validates that the requested title is not empty.
+    ///	@param _title The title of the book to be borrowed.
+    ///	@dev returns the error "The requested book does not exist." If the book ID is not valid.
+    ///	@dev returns the error "There are no more available copies of this book." If all copies of the book have been borrowed.
+    ///	@dev returns the error "You have already borrowed this book." If the user has already borrowed this book.
+    function borrowBook(string memory _title) public validBookTitle(_title) {
+        (bool exists, bytes32 bookKey, Book storage book) = findBook(_title);
+
+        require(exists, "The requested book does not exist.");
+
+        require(
+            book.copies > 0,
+            "There are no more available copies of this book."
+        );
+
+        require(
+            !borrowedBook[msg.sender][bookKey],
+            "You have already borrowed this book."
+        );
+
+        // mark book as borrowed by the borrower
+        borrowedBook[msg.sender][bookKey] = true;
+        // decrease number of copies to indicate availability
+        book.copies--;
+        // Given there's no requirement for uniqueness we don't need to check if the address is already added to the array
+        book.borrowers.push(msg.sender);
+
+        emit BookBorrowed(_title, msg.sender, book.copies);
+    }
+
+    /// @dev Allows a user to return a book they have borrowed from the library.
+    /// @param _title The title of the book to be returned. It cannot be empty.
+    /// @notice retuurns the error:  "The requested book does not exist." If the book ID is not valid.
+    /// @notice retuurns the error: "You are not borrowing this book." If the user is not currently borrowing the book.
+    function returnBook(string memory _title) public validBookTitle(_title) {
+        (bool exists, bytes32 bookKey, Book storage book) = findBook(_title);
+
+        require(exists, "The requested book does not exist.");
+
+        require(
+            borrowedBook[msg.sender][bookKey],
+            "You have not borrowed this book."
+        );
+
+        // Book is no longer borrowed by this address
+        borrowedBook[msg.sender][bookKey] = false;
+        // increase number of copies to update availability
+        book.copies++;
+
+        emit BookReturned(_title, msg.sender, book.copies);
+    }
+
+    /// @dev Allows users to retrieve the borrowers of a given book
+    /// @param _title The title of the book.
+    /// @notice returns An array of addresses representing the borrowers of the book.
+    /// @notice returns the error: "The requested book does not exist." If the book ID is not valid.
+    function getBorrowers(string memory _title)
+        public
+        view
+        validBookTitle(_title)
+        returns (address[] memory borrowers) 
+    {
+        (bool exists, , Book storage book) = findBook(_title);
+
+        require(exists, "The requested book does not exist.");
+        return book.borrowers;
+    }
+
+    /**
+     * @notice  returns the length of the bookKeys array which represents the number of unique books (different book titles) available in the library
+     * @dev     this number should be used to obtain all keys from the bookKeys array and then retrieve el the books from the books mapping
+     */
+    function getNumberOfBooks() external view returns (uint256 _numberOfBooks) {
+        return bookKeys.length;
+    }
 	
-	///	@dev Allows a user to borrow a book from the library.
-	///	@param _bookId The ID of the book to be borrowed.
-	///	@notice returns the error "The requested book does not exist." If the book ID is not valid.
-	///	@notice returns the error "There are no more available copies of this book." If all copies of the book have been borrowed.
-	///	@notice returns the error "You have already borrowed this book." If the user has already borrowed this book.
-	function borrowBook(uint _bookId) public {
-		require(_bookId < bookCount, "The requested book does not exist.");
-		Book storage book = books[_bookId];
-		require(book.borrowedCopies < book.copies, "There are no more available copies of this book.");
-		require(!currentBookBorrowers[_bookId][msg.sender], "You have already borrowed this book.");
-		currentBookBorrowers[_bookId][msg.sender] = true;
-		book.borrowedCopies++;
-		
-		// Only add to the history of borrowers if it's the first time that is borrowing a given  book
-		if (!historicalBookBorrowers[_bookId][msg.sender] ) {
-			historicalBookBorrowers[_bookId][msg.sender] = true;
-			book.borrowers.push(msg.sender);
-		}	
-	}
+    /**
+     * @notice  gets the data associated to a book by its title
+     * @dev     validates that the title is not an empty string, converts the title to bytes32 to get the book key
+     * @param   _title  the title of the requested book
+     * @return  requestedBook  a struct with the title of the book, number of copies and an array of borrowers
+     */
+    function getBookByTitle(string memory _title)
+        external
+        view
+        validBookTitle(_title)
+        returns (
+            Book memory requestedBook
+        )
+    {
+        bytes32 bookKey = keccak256(abi.encodePacked(_title));
+        requestedBook = books[bookKey];
+    }
 
-	/// @dev Allows a user to return a book they have borrowed from the library.
-	/// @param _bookId The ID of the book to be returned.
-	/// @notice retuurns the error:  "The requested book does not exist." If the book ID is not valid.
-	/// @notice retuurns the error: "You are not borrowing this book." If the user is not currently borrowing the book.
-	function returnBook(uint _bookId) public {
-		require(_bookId < bookCount, "The requested book does not exist.");
-		Book storage book = books[_bookId];
-		require(currentBookBorrowers[_bookId][msg.sender], "You have not borrowed this book.");
-		currentBookBorrowers[_bookId][msg.sender] = false;
-		book.borrowedCopies--;
-	}
-
-	/// @dev Allows the user to view a list of borrowers for a specific book.
-	/// @param _bookId The ID of the book to view the borrowers of.
-	/// @notice returns An array of addresses representing the borrowers of the book.
-	/// @notice returns the error: "The requested book does not exist." If the book ID is not valid.
-	function viewBorrowers(uint _bookId) public view returns (address[] memory) {
-		require(_bookId < bookCount, "The requested book does not exist.");
-		return books[_bookId].borrowers;
-	}
+	 /**
+     * @notice  gets the data associated to a book by its title
+     * @dev     validates that the title is not an empty string, converts the title to bytes32 to get the book key
+     * @param   _bookKey the key of the book to be returned in bytes32 format
+     * @return  requestedBook  a struct with the title of the book, number of copies and an array of borrowers
+     */
+    function getBookByKey(bytes32  _bookKey)
+        external
+        view
+        returns (
+            Book memory requestedBook
+        )
+    {
+        requestedBook = books[_bookKey];
+    }
 }
--- tests/BookLibrary_test.sol
+++ tests/BookLibrary_test.sol
@@ -1,20 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
         
-pragma solidity ^0.8.0;
+pragma solidity 0.8.17;
 
 import "remix_tests.sol"; 
 import "remix_accounts.sol";
 import "../contracts/BookLibrary.sol";
 
 /// @dev Tests for the BookLibrary contract
-contract BookLibraryTests {
+contract BookLibraryTests is BookLibrary {
     /// Define variables referring to different accounts
     address public acc0;
     address public acc1;
     address public acc2;
-
-    BookLibrary public bookLibrary;
-    address public owner;
     
     /// Initiate accounts variable
     function beforeAll() public {
@@ -23,62 +20,103 @@ contract BookLibraryTests {
         acc2 = TestsAccounts.getAccount(2);
     }
 
-    /// #sender: account-0 (deployer will always be 0)
-    function beforeEach() public {
-        bookLibrary = new BookLibrary();
-        owner = address(bookLibrary.owner());
-    }
 
     // @dev Test to add a book to the library. Check if the book is added successfully.
     function testAddBook() public {
-        bookLibrary.addBook("One Hundred Years of Solitude", 5);
-        string[] memory titles;
-        uint[] memory copies;
-        (titles, copies) = bookLibrary.viewBooks();
-        Assert.equal(titles[0], "One Hundred Years of Solitude", "The title of the book is not correct");
-        Assert.equal(copies[0], 5, "The number of copies is not correct");
+        addBook("One Hundred Years of Solitude");
+        addBook("One Hundred Years of Solitude");
+        addBook("One Hundred Years of Solitude");
+        addBook("One Hundred Years of Solitude");
+        addBook("One Hundred Years of Solitude");
+        
+        Book memory book = this.getBookByTitle("One Hundred Years of Solitude");
+        Assert.equal(book.title, "One Hundred Years of Solitude", "The title of the book is not correct");
+        Assert.equal(book.copies, 5, "The number of copies is not correct");
     }
 
-    /// #sender: account-1 (sender is account at index '1')
-    function addBookWithAccount1(string memory _title, uint _id) private {
-        bookLibrary.addBook(_title, _id);
-    }
+    /// #sender: account-1
+    function testOnlyOnwerCanAddBooks() public {
+        string memory title = "Rayuela";
+        addBook(title);
 
+        // The transaction will be reverted but we can't test this as in order to call try/catch we need to call the function
+        // externally which will lose the contex   
+    }
 
     /// @dev Test to view the books in the library. Check if the book list is correct.
-    function testViewBooks() public {
-        bookLibrary.addBook("One Hundred Years of Solitude", 5);
-        bookLibrary.addBook("1984", 4);
-        string[] memory titles;
-        uint[] memory copies;
-        (titles, copies) = bookLibrary.viewBooks();
-        Assert.equal(titles[0], "One Hundred Years of Solitude", "The first book title is not correct");
-        Assert.equal(titles[1], "1984", "The second book title is not correct");
-        Assert.equal(copies[0], 5, "The number of copies of the first book is not correct");
-        Assert.equal(copies[1], 4, "The number of copies of the second book is not correct");
+    function testGetAllBooks() public {
+        // Add 3 copies of a new book
+        addBook("1984");
+        addBook("1984");
+        addBook("1984");
+
+        // Add 1 copy of a new book
+        addBook("Pride and Prejudice");
+
+        uint numberOfBooks = this.getNumberOfBooks();
+        Book[] memory bookList = new Book[](numberOfBooks);
+
+        for (uint i=0; i < numberOfBooks; i++) {
+            bytes32 bookKey = this.bookKeys(i);
+            Book memory book = this.getBookByKey(bookKey);
+            bookList[i] = book;
+        }
+        
+        Assert.equal(bookList[0].title, "One Hundred Years of Solitude", "The first book title is not correct");
+        Assert.equal(bookList[1].title, "1984", "The second book title is not correct");
+        Assert.equal(bookList[2].title, "Pride and Prejudice", "The third book title is not correct");
+        Assert.equal(bookList[0].copies, 5, "The number of copies of the first book is not correct");
+        Assert.equal(bookList[1].copies, 3, "The number of copies of the second book is not correct");
+        Assert.equal(bookList[2].copies, 1, "The number of copies of the third book is not correct");
     }
     
-    // @dev Test to borrow a book from the library. Check if the book is borrowed successfully.
+
+    /// #sender: account-1
+    /// @dev Test to borrow a book from the library. Check if the book is borrowed successfully.
     function testBorrowBook() public {
-        bookLibrary.addBook("One Hundred Years of Solitude", 5);
-        uint bookId = 0;
-        bookLibrary.borrowBook(bookId);   
-        bool isBorrowed  = bookLibrary.currentBookBorrowers(bookId, bookLibrary.owner());
-        Assert.equal(isBorrowed, true, "The book is not borrowed");
+        string memory title = "One Hundred Years of Solitude";
+        // get key
+        bytes32 bookKey = keccak256(abi.encodePacked(title));
+
+        borrowBook(title);   
+
+        bool isBorrowedAcc1 = this.borrowedBook(acc1, bookKey);
+        Assert.equal(isBorrowedAcc1, true, "The book is not borrowed by account1");
     }
 
     // @dev Test to borrow a book from the library. Check if the book is borrowed successfully.
     function testDoubleBorrowingShouldFail() public {
-        bookLibrary.addBook("One Hundred Years of Solitude", 5);
-        uint bookId = 0;
-        bookLibrary.borrowBook(bookId);   
-        try bookLibrary.borrowBook(bookId) {
+        string memory title = "1984";
+        // calling externally to be able to use try. Using the contract address as caller
+        this.borrowBook(title);   
+        
+        // calling externally to be able to use try
+        try this.borrowBook(title) {
             Assert.ok(false, "method execution should fail");
         } catch Error(string memory reason) {
             // Compare failure reason, check if it is as expected
             Assert.equal(reason, "You have already borrowed this book.", "failed with unexpected reason");
         } catch (bytes memory /*lowLevelData*/) {
-            Assert.ok(false, 'failed unexpected');
+            Assert.ok(false, "Failed unexpectedly");
+        }
+    }
+    
+    ///  #sender: account-3
+    function testNoMoreCopiesAvailable() public {
+        string memory title = "Pride and Prejudice";
+        borrowBook(title);
+     
+        // Now all copies should be borrowed and the function should revert
+
+        // calling externally to be able to use try and to use a different address
+        // this is gas inneficient but is acceptable for testing purposes
+        try this.borrowBook(title) {
+            Assert.ok(false, "method execution should fail");
+        } catch Error(string memory reason) {
+            // Compare failure reason, check if it is as expected
+            Assert.equal(reason, "There are no more available copies of this book.", "failed with unexpected reason");
+        } catch (bytes memory /*lowLevelData*/) {
+            Assert.ok(false, "Failed unexpectedly");
         }
     }
 }
